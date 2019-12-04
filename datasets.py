ca1f21342f96f0cd15f0f8ebd936df61df9c4291
Clockers <46.lore.46@gmail.com>
2019-12-04 12:10:44 +0100

    added dataset link

---
+++ datasets.py
@@ -0,0 +1,8 @@
+'''
+datasets:
+
+arxiv-HepPh (arxiv) from the arxiv networks, in which each temporal edge indicates two authors having a common publication at
+a specified time instance
+http://konect.uni-koblenz.de/networks/ca-cit-HepPh
+
+'''
