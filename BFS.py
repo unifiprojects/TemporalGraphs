bef8c934159c87c878fb7c2b994d47ab7d53c9c5
Matteo Mauro <mauro.matteo96@gmail.com>
2019-12-15 16:07:51 +0100

    b

--- BFS.py
+++ BFS.py
@@ -110,7 +110,7 @@ while queue:
         # il predecessore nella BFS non deve essere considerato come vicino
         if v is predecessor[current_node]:
             continue
-
+        print("\tHo scelto il nodo vicino: " + v)
         # filtra gli archi che non sono stati attraversati e che sono ancora attivi
         filter_function = lambda edge: not edge.is_traversed and sigma[current_node] <= edge.time
         E = list(filter(filter_function, graph.get_edge_neighbor(current_node, v)))
@@ -123,6 +123,7 @@ while queue:
                     new_tree_node = TreeNode(v, edge_min.time)
                     tree_nodes[v] = new_tree_node
                     current_tree_node.add_node(new_tree_node)
+                    print("\tAggiungo a " + current_tree_node.name + " il figlio " + v)
                     dist[v] = dist[current_node] + 1
                     sigma[v] = edge_min.time
                     predecessor[v] = current_node
@@ -134,6 +135,7 @@ while queue:
                         new_tree_node = TreeNode(v, edge_min.time)
                         tree_nodes[v] = new_tree_node
                         current_tree_node.add_node(new_tree_node)
+                        print("\tAggiungo a " + current_tree_node.name + " il figlio " + v)
                         sigma[v] = edge_min.time
                         predecessor[v] = current_node
                         updateNodeIntoQueue(queue, occurrence, sigma[v], current_node)
@@ -143,6 +145,7 @@ while queue:
                         new_tree_node = TreeNode(v, edge_min.time)
                         tree_nodes[v] = new_tree_node
                         current_tree_node.add_node(new_tree_node)
+                        print("\tAggiungo a " + current_tree_node.name + " il figlio " + v)
                         dist[v] = dist[current_node] + 1
                         sigma[v] = edge_min.time
                         predecessor[v] = current_node
@@ -156,7 +159,7 @@ print(tree_node_root)
 print(tree_node_root.list_of_nodes[0])
 print(tree_node_root.list_of_nodes[1])
 print(tree_node_root.list_of_nodes[2])
-#draw_tree(tree_node_root)
+draw_tree(tree_node_root)
 
 
 new_tree_node = TreeNode(v, edge_min.time)
