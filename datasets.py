9ce5793bea6e83ac6e62cf60520bf44592038dc1
Matteo Mauro <mauro.matteo96@gmail.com>
2019-12-30 17:52:38 +0100

    deleted unused file

--- datasets.py
+++
@@ -1,8 +0,0 @@
-'''
-datasets:
-
-arxiv-HepPh (arxiv) from the arxiv networks, in which each temporal edge indicates two authors having a common publication at
-a specified time instance
-http://konect.uni-koblenz.de/networks/ca-cit-HepPh
-
-'''
